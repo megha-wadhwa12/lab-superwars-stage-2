aa37fb5b28c74922ab1481f09111b70af1d754a5
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2023-10-23 14:37:44 +0530

    superwars-2

--- src/app.js
+++ src/app.js
@@ -26,6 +26,24 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for (let i = 0; i < players.length; i++) {
+        let hero_or_villain = i%2==0 ? "hero" : "villain"
+        // if(i%2==0){
+        //     let hero_or_villain = "hero"
+        // }
+        // else{
+        //     let hero_or_villain = "villain"
+        // }
+
+        let obj = {
+            name:players[i],
+            strength: getRandomStrength(),
+            image:`images/super-${i+1}.png` ,
+            type:hero_or_villain
+        }
+        detailedPlayers.push(obj)
+        
+    }
 
     return detailedPlayers;
 }
@@ -34,6 +52,8 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    var strength = Math.ceil(Math.random()*100)
+    return strength
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +62,15 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for(let i=0;i<players.length;i++){
+      if(players[i].type == type){
+        fragment += `<div class="player">
+        <img src="${players[i].image}" alt="">
+        <div class="name">${players[i].name}</div>
+        <div class="strength">${players[i].strength}</div>
+     </div>`
+      }
+    }
 
     return fragment;
 }
